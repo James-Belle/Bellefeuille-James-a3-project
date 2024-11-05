17c63f147036b7e8bfa2111764e9e2946f77711b
James-Belle <jamesbelle549@gmail.com>
2024-11-05 14:01:01 -0500

    add basic interface and debris system (subject to change)

---
+++ Bellefeuille-James-Ice-Game-a3/Debris.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Numerics;
+
+
+namespace Game10003
+{
+	public class Debris
+	{
+		public Vector2 debrisPosition;
+
+		Vector2 speed;
+		int size = 15;
+
+
+		public Debris()
+		{
+			
+            speed.X = Random.Float((float)-2, (float)-1);
+            speed.Y = Random.Float((float)-1, (float)1);
+            debrisPosition.X = 700;
+			debrisPosition.Y = 300;
+        }
+
+		public void DebrisSpawn()
+		{
+
+		}
+
+		public void DebrisMove()
+		{
+			debrisPosition += speed;
+			if (debrisPosition.X < 200 || debrisPosition.Y < 0 || debrisPosition.Y > 600)
+			{
+				debrisPosition.X = 700;
+				debrisPosition.Y = 300;
+                speed.X = Random.Float((float)-2, (float)-1);
+                speed.Y = Random.Float((float)-1, (float)1);
+            }
+			Draw.Circle(debrisPosition, size);
+		}
+		public bool DebrisCollide(Vector2 oldMousePosition, Vector2 newMousePosition, bool switcher)
+		{
+			
+			if (Math.Abs(debrisPosition.Y-newMousePosition.Y) < size + 15 && Math.Abs(debrisPosition.X-newMousePosition.X) < size+25 && !switcher)
+			{
+				Console.WriteLine("colide");
+				speed += (newMousePosition - oldMousePosition) /15;
+				return (true);
+            }
+			return (switcher);
+		}
+	}
+}
--- Bellefeuille-James-Ice-Game-a3/Game.cs
+++ Bellefeuille-James-Ice-Game-a3/Game.cs
@@ -1,6 +1,8 @@
 ﻿// Include code libraries you need below (use the namespace).
+using Raylib_cs;
 using System;
 using System.Numerics;
+using System.Threading;
 
 // The namespace your code is in.
 namespace Game10003
@@ -11,14 +13,21 @@ namespace Game10003
     public class Game
     {
         // Place your variables here:
+        Vector2 mouseBrush = new Vector2(50, 10);
+        Debris deb = new Debris();
+        Vector2 lastMousePos = new Vector2(0,0);
+        int timer = 0;
+        bool colide = false;
 
 
+        Vector2 storedMousePos = new Vector2(0,0);
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
-
+            Window.SetSize(800, 600);
+            
         }
 
         /// <summary>
@@ -26,7 +35,52 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
+            Window.ClearBackground(Color.White);
+            Draw.Rectangle(0, 0, 200, 600);
 
+            Vector2 mousePosition = Input.GetMousePosition();
+
+
+            //
+            // collision check here
+            colide = deb.DebrisCollide(lastMousePos, mousePosition, colide);
+            if (!colide)
+            {
+                storedMousePos = lastMousePos;
+
+            }
+            else
+            {
+                Draw.LineColor = Color.Red;
+                Draw.Capsule(lastMousePos, storedMousePos, 5); // this shows the trajectory of your mouse colliding with the debris
+                Draw.LineColor = Color.Black;
+                timer++;
+            }
+
+
+            //
+            //
+            if (timer > 20)
+            {
+                timer = 0;
+                Console.WriteLine(mousePosition - lastMousePos);
+                colide = false;
+            }
+            
+            
+
+            if (mousePosition.X > 200)
+            {
+                Draw.Ellipse(mousePosition, mouseBrush);
+            }
+
+            deb.DebrisMove();
+
+            if (!colide)
+            {
+                lastMousePos = mousePosition;
+            }
+            
         }
     }
 }
